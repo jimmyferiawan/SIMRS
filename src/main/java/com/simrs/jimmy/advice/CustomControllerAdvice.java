e15d0f0221f918bca6c96a866b284b8a6e74effb
jimmyferiawan <jimyferi1000>
2022-06-11 20:22:44 +0700

    add logging to exception handler

--- src/main/java/com/simrs/jimmy/advice/CustomControllerAdvice.java
+++ src/main/java/com/simrs/jimmy/advice/CustomControllerAdvice.java
@@ -3,6 +3,7 @@ package com.simrs.jimmy.advice;
 import com.simrs.jimmy.dto.response.BaseResponse;
 import com.simrs.jimmy.dto.response.format.ErrorResponse;
 import com.simrs.jimmy.dto.response.format.FieldValidResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @ControllerAdvice
 @RestController
+@Slf4j
 public class CustomControllerAdvice {
 
     private BaseResponse baseResponse;
@@ -28,7 +30,7 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleJsonFormat(HttpMessageNotReadableException ex) {
-        System.out.println("handleJsonFormat(): error format JSON");
+        log.info("handleJsonFormat(): error format JSON");
         baseResponse = new BaseResponse();
         baseResponse.setMessage("error format JSON");
         return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
@@ -37,6 +39,7 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<BaseResponse> customHandleValidation(MethodArgumentNotValidException ex) {
+        log.info("Validation request error");
 //        System.out.println("status.name(): "+ status.name() + ", status.value(): " + status.value());
         List<FieldValidResponse> validationErrors = new ArrayList<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> {
@@ -56,7 +59,7 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<BaseResponse> customEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
-        System.out.println("EntityNotFoundException.class");
+        log.info("EntityNotFound");
         baseResponse = new BaseResponse(false, "Data dengan id " + ex.getMessage() + " tidak ditemukan");
 
         return new ResponseEntity<>(baseResponse, HttpStatus.NOT_FOUND);
@@ -65,7 +68,7 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(EntityExistsException.class)
     public ResponseEntity<BaseResponse> customEntityExistsException(EntityExistsException ex, WebRequest request) {
-        System.out.println("EntityExistsException.class");
+        log.info("EntityExists");
         baseResponse = new BaseResponse(false, "Data dengan id " + ex.getMessage() + " sudah ada");
 
         return new ResponseEntity<>(baseResponse, HttpStatus.CONFLICT);
@@ -74,7 +77,7 @@ public class CustomControllerAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public ResponseEntity<BaseResponse> customSqlIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex, WebRequest request) {
-        System.out.println("SQLIntegrityConstraintViolationException.class");
+        log.info("SQLIntegrityConstraintViolationException.class");
         baseResponse = new BaseResponse(false, "Data dengan id " + ex.getMessage() + " sudah ada");
 
         return new ResponseEntity<>(baseResponse, HttpStatus.CONFLICT);
